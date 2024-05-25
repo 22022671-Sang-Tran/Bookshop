d270d5d65173d13d71d63dc31f30b3084fe117c8
22022671-Sang-Tran <22022671@vnu.edu.vn>
2024-05-25 05:11:58 +0000

    update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -13,8 +13,9 @@ import os
 import dj_database_url
 from pathlib import Path
 from environs import Env # new
+
 env = Env() # new
-env.read_env() 
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -168,4 +169,10 @@ LOGOUT_REDIRECT_URL = 'index'
 
 
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AWS_ACCESS_KEY_ID = 'IgkMk6wlV2zLH27IoJBtgK38vw6Ccy9qWV/SrkgP'
+AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
+AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
+AWS_S3_REGION_NAME = 'your-region'  # e.g., 'us-east-1'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
