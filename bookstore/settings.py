339597a1b66f7fc21a379a8cdf851dfbd6135f5d
22022671-Sang-Tran <22022671@vnu.edu.vn>
2024-05-25 14:36:10 +0000

    change

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -13,10 +13,8 @@ import os
 import dj_database_url
 from pathlib import Path
 from environs import Env # new
-from decouple import config
-
 env = Env() # new
-env.read_env()
+env.read_env() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -170,14 +168,4 @@ LOGOUT_REDIRECT_URL = 'index'
 
 
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_SIGNATURE_NAME = config('AWS_S3_SIGNATURE_NAME', default='s3v4')
-AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME')
-AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE', cast=bool, default=False)
-AWS_DEFAULT_ACL = config('AWS_DEFAULT_ACL', default=None)
-AWS_S3_VERIFY = config('AWS_S3_VERIFY', cast=bool, default=True)
-DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE', default='storages.backends.s3boto3.S3Boto3Storage')
-STATICFILES_STORAGE = config('STATICFILES_STORAGE', default='storages.backends.s3boto3.S3Boto3Storage')
-AWS_LOCATION = config('AWS_LOCATION', default='static')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
